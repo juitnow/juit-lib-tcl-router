5407e0a61048aed4c653f025c99e16863d89ea7b
Pier Fumagalli <pier@usrz.com>
2023-03-21 19:48:11 +0100

    Disable coverage if tests can not be run

--- build.ts
+++ build.ts
@@ -1,3 +1,11 @@
 import { tasks } from '@plugjs/build'
+import * as dotenv from 'dotenv'
 
-export default tasks()
+dotenv.config()
+const enableCoverage = !! process.env.ALCATEL_HOST
+
+export default tasks({
+  coverage: enableCoverage,
+  minimumCoverage: enableCoverage ? 100 : 0,
+  minimumFileCoverage: enableCoverage ? 100 : 0,
+})
